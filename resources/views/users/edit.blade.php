a789f5afe73d838dee24033aaf781fc42ba376a4
rickydev254 <chumamaeric@gmail.com>
2024-03-08 13:36:17 +0300

    added margin

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -11,12 +11,12 @@
             @method('PUT')
 
             <div class="form-group">
-                <label for="name">Name:</label>
+                <label for="name">Name</label>
                 <input type="text" id="name" name="name" value="{{ $user->name }}" class="form-control">
             </div>
 
-            <div class="form-group">
-                <label for="email">Email:</label>
+            <div class="form-group mt-3">
+                <label for="email">Email</label>
                 <input type="email" id="email" name="email" value="{{ $user->email }}" class="form-control">
             </div>
 
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,11 +6,11 @@
         <div class="card-header">User Details</div>
         <div class="card-body">
             <div class="form-group">
-                <label for="name">Name:</label>
+                <label for="name">Name</label>
                 <input type="text" id="name" value="{{ $user->name }}" class="form-control" readonly>
             </div>
-            <div class="form-group">
-                <label for="email">Email:</label>
+            <div class="form-group mt-3">
+                <label for="email">Email</label>
                 <input type="email" id="email" value="{{ $user->email }}" class="form-control" readonly>
             </div>
             <!-- Add other user details as needed -->
